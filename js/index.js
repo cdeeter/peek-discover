d4cb85d0b03f0383421644f008409138307212fe
Celia <celia@peek.com>
2016-06-06 18:51:41 -0700

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,74 @@
+var homeController = (function(){
+  var selectors = {};
+
+  return {
+    init: function() {
+      selectors.homePage         = $("#home-page");
+      selectors.signupEmailField = $("#signup-email");
+      selectors.loginEmailField  = $("#login-email");
+      selectors.signupButton     = $("#signup-button");
+      selectors.loginButton      = $("#login-button");
+      selectors.loginForm        = $("#home-page #login-form");
+      selectors.signupForm       = $("#signup-form");
+      selectors.actionLinks      = $("#action-links");
+      selectors.signupLink       = $("#signup-link");
+      selectors.loginLink        = $("#login-link");
+      selectors.switchToSignup   = $("#or-signup-link");
+      selectors.switchToLogin    = $("#or-login-link");
+      this.bindUIActions();
+    },
+    bindUIActions: function() {
+      selectors.signupButton.click(function(e) {
+        var email = selectors.signupEmailField.val();
+
+        e.preventDefault();
+        this.signUp(email);
+      }.bind(this));
+
+      selectors.loginButton.click(function(e) {
+        var email = selectors.loginEmailField.val();
+
+        e.preventDefault();
+        this.logIn(email);
+      }.bind(this));
+
+      selectors.signupLink.click(function () {
+        this.showSection(selectors.signupForm, selectors.actionLinks);
+      }.bind(this));
+
+      selectors.loginLink.click(function () {
+        this.showSection(selectors.loginForm, selectors.actionLinks);
+      }.bind(this));
+
+      selectors.switchToSignup.click(function () {
+        this.showSection(selectors.signupForm, selectors.loginForm);
+      }.bind(this));
+
+      selectors.switchToLogin.click(function () {
+        this.showSection(selectors.loginForm, selectors.signupForm);
+      }.bind(this));
+    },
+    hideSection: function(selector) {
+      selector.hide();
+    },
+    showSection: function(sectionToShow, sectionToHide) {
+      var findShowSelector = selectors.homePage.find(sectionToShow);
+      var findHideSelector = selectors.homePage.find(sectionToHide);
+
+      this.hideSection(findHideSelector);
+      findShowSelector.show();
+    },
+    signUp: function (email) {
+      window.sessionStorage.setItem('peekDiscoverEmail', email);
+      window.location.assign('/sign_up.html');
+    },
+    logIn: function(email) {
+      window.sessionStorage.setItem('peekLoginEmail', email);
+      window.location.assign('/profile.html');
+    },
+  }
+})();
+
+$(document).ready(function () {
+  homeController.init();
+});
---
+++ server.js
@@ -0,0 +1,46 @@
+var http           = require('http');
+var express        = require('express');
+var nodemailer     = require('nodemailer');
+var fs             = require('fs');
+var bodyParser     = require('body-parser');
+var swig           = require('swig');
+var EmailTemplates = require('swig-email-templates');
+
+var app = express();
+
+app.use('/', express.static(__dirname + '/'));
+app.use(bodyParser.urlencoded());
+
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+app.post('/email', function (req, res) {
+  var directConfig = 'direct:?name=Gmail';
+  var transporter = nodemailer.createTransport(directConfig);
+  var templates = new EmailTemplates({root: __dirname});
+  var signupTemplate = 'signup_email.html'
+  var context = {
+    firstName: req.body.firstName
+  };
+
+  templates.render('views/signup_email.html', context, function(err, html, text) {
+    if (err) {
+      res.status('500');
+      return console.log(err);
+    }
+
+    transporter.sendMail({
+      from: 'test@peek.com',
+      to: req.body.email,
+      subject: 'Welcome to Peek | Discover!',
+      html: html
+    });
+  });
+
+  res.status('200').send('Success!');
+});
+
+
+app.listen(8080);
